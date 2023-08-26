fbce684c75758fe425ce80f6681a14e4f9a972b3
YousefKullab <the1yousef98@gmail.com>
2023-08-26 15:54:51 +0300

    style the index view

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import { handleSubmit } from './js/app'
+import { countDown } from './js/app'
+
+import './styles/base.scss'
+import './styles/form.scss'
+
+export{
+    handleSubmit,
+    countDown
+}
---
+++ src/client/js/app.js
@@ -0,0 +1,7 @@
+function handleSubmit(){
+
+}
+
+function countDown(){
+    
+}
