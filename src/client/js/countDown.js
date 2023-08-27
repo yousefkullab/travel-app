f32c69c834a5a6449ae33cbd9ad686dd06d8b487
YousefKullab <the1yousef98@gmail.com>
2023-08-28 00:38:20 +0300

    create jest test for server and client side

--- src/client/js/countDown.js
+++ src/client/js/countDown.js
@@ -1,7 +1,10 @@
 function countDown(date){
+    // get the current date
     let today = new Date();
+    // get the travel date
     const travel_date = new Date(date);
     const time_left = travel_date.getTime() - today.getTime()
+    // 1 Day = 24 Hours = 24 * 60 Minutes = 24 * 60 * 60 Seconds
     const days_left = Math.ceil(time_left / (1000 * 3600 * 24));
     return days_left 
 }
--- src/server/server.js
+++ src/server/server.js
@@ -92,5 +92,8 @@ app.post('/travel-data', async (req, res) =>{
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!');
 });
+
+
+module.exports = app;
